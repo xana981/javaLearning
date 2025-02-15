452442f77052def7c5c42f07ff92ff795fa8c05e
sari <sari@DESKTOP-U7TGTT1>
2025-02-15 17:06:32 +0100

    añadiendo una funcion al ejercicio

--- javaLearning/src/javaLearning/cap02/Ejercicio02_02.java
+++ javaLearning/src/javaLearning/cap02/Ejercicio02_02.java
@@ -1,11 +1,14 @@
 package javaLearning.cap02;
 
 public class Ejercicio02_02 {
+	private static int sumaFrutas(int a, int b) {
+		return a+b;
+	}
 	public static void main(String[] args) {
 		int numManzanas = 5;
 		int numPeras = 4;
-		int numFrutas = numPeras+numManzanas;
-		System.out.println("el frutero tiene "+ numFrutas );
+		
+		System.out.println("el frutero tiene "+ sumaFrutas(numManzanas, numPeras)+ " frutas") ;
 		float numeroPi = 3.14f;
 		String saluda = "hola a todos"; //el tipo String no es un tipo de datos simple, es una clase con sus metodos.
 		System.out.println ("el  numero pi es "+ numeroPi);
