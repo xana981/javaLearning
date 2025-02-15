c26b485c368274c22128bb9670ea96531c851284
sari <sari@DESKTOP-U7TGTT1>
2025-02-15 16:57:48 +0100

    primer commit

---
+++ javaLearning/src/javaLearning/cap01/Ejemplo01_01.java
@@ -0,0 +1,8 @@
+package javaLearning.cap01;
+
+public class Ejemplo01_01 {
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+	}
+
+}
---
+++ javaLearning/src/javaLearning/cap02/Ejercicio02_02.java
@@ -0,0 +1,15 @@
+package javaLearning.cap02;
+
+public class Ejercicio02_02 {
+	public static void main(String[] args) {
+		int numManzanas = 5;
+		int numPeras = 4;
+		int numFrutas = numPeras+numManzanas;
+		System.out.println("el frutero tiene "+ numFrutas );
+		float numeroPi = 3.14f;
+		String saluda = "hola a todos"; //el tipo String no es un tipo de datos simple, es una clase con sus metodos.
+		System.out.println ("el  numero pi es "+ numeroPi);
+		System.out.println ("saludandote "+ saluda);
+	}
+
+}
